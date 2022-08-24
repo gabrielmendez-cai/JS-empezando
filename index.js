f3addbd681f75819bf97364f19b93c2f16868478
Gabriel Mendez <gabrielmendez1109@gmail.com>
2022-08-23 21:30:18 -0300

    ej de FOR y WHILE

---
+++ index.js
@@ -0,0 +1,36 @@
+
+/*
+let club = prompt("ingrese club del cual es hincha");
+if ((club !=="") && (club === "independiente" || club === "INDEPENDIENTE")){
+
+alert("sos un crack")}
+else{ alert( "huira muerto")};
+*/
+/*
+let usuario = parseFloat( prompt("ingrese usuario"));
+let contraseña = prompt("contraseña");
+if (usuario.toLowerCase === "gaby" &&  contraseña  === "090588" ) {alert ("ingresaste");} else {alert ("usuario y/o contraseña incorrecta");}
+*/
+
+/*for(let i = 0; i < 20; i++){
+    alert ("hola gaby mendez")
+}
+*/
+
+/*let agua = parseInt(prompt("ingrese cantidad de litros de agua a comprar o para salir ingrese 100"));
+for (let i = 99 ; i < 100; i++){
+let pesos = agua * i ;
+let total = `${agua} * ${i} = ${pesos}` ;
+alert(total +" pesos")
+}
+*/
+
+let adivinanza = prompt("no muerde, ni ladra, pero la casa guarda. que es?");
+while (adivinanza != "la llave"){
+    alert ("es incorrecto, volve a intentarlo");
+    adivinanza = prompt("no muerde, ni ladra, pero la casa guarda. que es?")
+}
+alert("muy bien adivinaste")
+
+    
+
